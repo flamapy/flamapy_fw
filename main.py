168c70f0ce0cdb32968dbfafacad2f99cba5a626
José Á. Galindo <jagalindo@us.es>
2020-07-08 18:58:12 +0200

    updating xml reader

--- main.py
+++ main.py
@@ -8,15 +8,12 @@ from pysat_metamodel.transformations.fm_to_pysat import Fm_to_pysat
 from pysat_metamodel.operations.Glucose3Valid import Glucose3Valid
 from pysat_metamodel.operations.Glucose3Products import Glucose3Products
 
-# Create a small fm manually //readers yet missing
-feature_b = Feature('B', [])
-relation = Relation(parent=None, children=[feature_b], card_min=0, card_max=1)
-feature_a = Feature('A', [relation])
-relation.parent = feature_a
-fm = FeatureModel(feature_a, [])
+# Parse a file
+xmlreader = XMLTransformation("/mnt/c/Users/jagal/Documents/Repositories/FaMaPy/core/test.fama")
+fm=xmlreader.transform()
 
-xmlreader = XMLTransformation('df-case1.fama')
-xmlreader.transform()
+#print the model
+print(fm)
 
 # Create a detination metamodel (Pysat for the record)
 sat= PySATModel()
@@ -32,7 +29,7 @@ valid = Glucose3Valid()
 valid.execute(sat)
 
 # Print the result
-print(valid.isValid())
+print("Is the model valid: "+ str(valid.isValid()))
 
 # Create the operation
 products = Glucose3Products()
@@ -41,4 +38,5 @@ products = Glucose3Products()
 products.execute(sat)
 
 # Print the result
+print("The products encoded in the model are: ")
 print(products.getProducts())
