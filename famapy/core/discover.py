2b7547183802c3ee6dc056cc7bca7b94ee95ad82
José A. Galindo <jagalindo@us.es>
2020-12-30 01:26:31 +0100

    Solving an issue that prevent loading operations by the operation name instead of the implementation name

--- famapy/core/discover.py
+++ famapy/core/discover.py
@@ -134,6 +134,7 @@ class DiscoverMetamodels:
         operation = plugin.use_operation(operation, variability_model)
         return operation.get_result()
 
+''' This is to be defined
     def use_operation_from_fm_file(
         self,
         plugin: str,
@@ -148,3 +149,4 @@ class DiscoverMetamodels:
         variability_model = plugin.use_transformation_m2m(vm_temp, file)
         operation = plugin.use_operation(operation, variability_model)
         return operation.get_result()
+'''
