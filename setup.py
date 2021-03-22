9b5949963acd3048822cb50878f661c33d6501aa
David Romero <davromorg@alum.us.es>
2021-03-22 20:54:07 +0100

    fix: Fixing "lOS" from "OS Independent"

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setuptools.setup(
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
-        "Operating System :: lOS Independent",
+        "Operating System :: OS Independent",
     ],
     python_requires='>=3.6',
     install_requires=[],
