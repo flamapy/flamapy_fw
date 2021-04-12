7608d54ee38864c7db01a577ed33842f00dcf866
GermanOctako <germanoctako@gmail.com>
2021-04-12 17:22:55 +0200

    Delete unnecessary abstract methods

--- famapy/core/operations/error_diagnosis.py
+++ famapy/core/operations/error_diagnosis.py
@@ -12,11 +12,3 @@ class ErrorDiagnosis(Operation):
     @abstractmethod
     def get_diagnosis_messages(self) -> list:
         pass
-
-    @abstractmethod
-    def set_dead_features(self, dead_features: list):
-        pass
-
-    @abstractmethod
-    def set_false_optional_features(self, false_optional_features: list):
-        pass
