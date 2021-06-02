815ee8aec58090effd16ee6cfe15ada2874eeecf
GermanOctako <germanoctako@gmail.com>
2021-06-02 19:49:07 +0200

    New operations abstract classes

--- famapy/core/operations/__init__.py
+++ famapy/core/operations/__init__.py
@@ -1,20 +1,21 @@
 from .abstract_operation import Operation
-
+from .average_branching_factor import AverageBranchingFactor
 from .commonality import Commonality
 from .core_features import CoreFeatures
+from .count_leafs import CountLeafs
 from .dead_features import DeadFeatures
-from .false_optional_features import FalseOptionalFeatures
 from .error_detection import ErrorDetection
 from .error_diagnosis import ErrorDiagnosis
+from .false_optional_features import FalseOptionalFeatures
+from .filter import Filter
 from .products import Products
+from .products_number import ProductsNumber
 from .valid import Valid
 from .valid_configuration import ValidConfiguration
 from .valid_product import ValidProduct
 from .variability import Variability
-from .count_leafs import CountLeafs
-from .average_branching_factor import AverageBranchingFactor
 
 __all__ = ["Commonality", "DeadFeatures", "CoreFeatures", "FalseOptionalFeatures", 
            "ErrorDetection", "ErrorDiagnosis", "Operation", "Products", "Valid", 
            "ValidConfiguration", "ValidProduct", "Variability", "CountLeafs", 
-           "AverageBranchingFactor"]
+           "AverageBranchingFactor", "ProductsNumber", "Filter"]
