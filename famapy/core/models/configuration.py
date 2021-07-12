79eee8b252c9820b4bbf907917c8454ff2ed0cc8
José Miguel Horcas Aguilera <miguelijordan@gmail.com>
2021-07-12 14:49:58 +0200

    Add equal, hash, and str for Configuration

--- famapy/core/models/configuration.py
+++ famapy/core/models/configuration.py
@@ -2,6 +2,20 @@ from typing import Any
 
 
 class Configuration():
+    """
+    A configuration is a selection of elements in a variability model.
+
+    It is represented as a dictionary of elements of Any type -> bool.
+    """
 
     def __init__(self, elements: dict[Any, bool]) -> None:
         self.elements = elements
+
+    def __eq__(self, other: 'Configuration') -> bool:
+        return self.elements == other.elements
+
+    def __hash__(self) -> int:
+        return hash(frozenset(self.elements.items()))
+
+    def __str__(self) -> str:
+        return str(self.elements)
