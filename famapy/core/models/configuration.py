3414f88772bd4fda35b01e0a88b5b25b8dbb3593
José Miguel Horcas Aguilera <miguelijordan@gmail.com>
2021-07-12 15:11:34 +0200

    eq method returns False for other instances

--- famapy/core/models/configuration.py
+++ famapy/core/models/configuration.py
@@ -12,9 +12,9 @@ class Configuration():
         self.elements = elements
 
     def __eq__(self, other: object) -> bool:
-        if not isinstance(other, Configuration):
-            return NotImplemented
-        return self.elements == other.elements
+        if isinstance(other, Configuration):
+            return self.elements == other.elements
+        return False
 
     def __hash__(self) -> int:
         return hash(frozenset(self.elements.items()))
