c7096e866097fa2a0a45a1cc8e12aca0de549d89
jagalindo <jagalindo@us.es>
2021-07-20 20:22:00 +0200

    updating the ast class with some aux methods

--- famapy/core/models/ast.py
+++ famapy/core/models/ast.py
@@ -12,28 +12,36 @@ class Node:
         return not self.is_op()
 
     def is_op(self):
-        return isinstance(self.data, str) and ( self.data in Node.operations )
+        #We check if the op is available by comparing it on mayus
+        return (isinstance(self.data, str) and 
+            ( self.data.upper() in (op.upper() for op in Node.operations )))
     
     def __str__(self):
-        return str(self.data)
-
+        if ( self.left and self.right):
+            return str(self.data) + '\r\n/ ' + '\\\r\n' +  str(self.left) +str(self.right) 
+        elif (self.left and not self.right):
+            return str(self.data) + '\r\n/ '  +  str(self.left) 
+        elif (not self.left and self.right):
+            return str(self.data) + '\\\r\n'  +str(self.right) 
+        else:
+            return str(self.data)
 class AST:
     
     def __init__(self, root):
         self.root = Node(root)
 
     @staticmethod
-    def create_binary_operation(op,left,right):
+    def create_simple_binary_operation(op,left,right):
         ast = AST(op)
         ast.root.left=Node(left)
         ast.root.right=Node(right)
         return ast
 
     @staticmethod
-    def create_unary_operation(op,elem):
+    def create_simple_unary_operation(op,elem):
         ast = AST(op)
         ast.left=elem
         return ast
 
     def __str__(self):
-        return str(self.root) + str(self.root.left) + str(self.root.right)
+        return str(self.root)
