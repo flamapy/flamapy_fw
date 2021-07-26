6599900949b9485cdf72afe069680ee31f84c67f
jagalindo <jagalindo@us.es>
2021-07-26 02:50:55 +0200

    updating the ast to print clearer

--- famapy/core/models/ast.py
+++ famapy/core/models/ast.py
@@ -2,7 +2,7 @@ from typing import Optional
 
 
 class Node:
-    operations = ['requires', 'excludes', 'and', 'or', 'implies', 'not']
+    operations = ['requires', 'excludes', 'and', 'or', 'implies', 'not', 'equivalence']
 
     def __init__(self, data: str):
         self.left: Optional['Node'] = None  # pylint: disable=unsubscriptable-object
@@ -21,32 +21,32 @@ class Node:
 
     def __str__(self) -> str:
         if self.left and self.right:
-            return f'{self.data}\r\n/ \\\r\n{self.left}{self.right}'
+            return f'{self.data}[{self.left}][{self.right}]'
 
         if self.left and not self.right:
-            return f'{self.data}\r\n/ {self.left}'
+            return f'{self.data}[{self.left}][]'
 
         if not self.left and self.right:
-            return f'{self.data}\\\r\n{self.right}'
+            return f'{self.data}[][{self.right}]'
 
         return str(self.data)
 
 
 class AST:
 
-    def __init__(self, root: str):
-        self.root = Node(root)
+    def __init__(self, root: Node):
+        self.root = root
 
     @classmethod
     def create_simple_binary_operation(cls, operation: str, left: str, right: str) -> 'AST':
-        ast = cls(operation)
+        ast = cls(Node(operation))
         ast.root.left = Node(left)
         ast.root.right = Node(right)
         return ast
 
     @classmethod
     def create_simple_unary_operation(cls, operation: str, elem: str) -> 'AST':
-        ast = cls(operation)
+        ast = cls(Node(operation))
         ast.root.left = Node(elem)
         return ast
 
