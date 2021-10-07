960d3294e454fe3f20e9673bad24f16006b9077c
José Miguel Horcas <jhorcas@us.es>
2021-10-07 18:53:01 +0200

    Convert AST into AST in CNF

--- famapy/core/models/ast.py
+++ famapy/core/models/ast.py
@@ -52,3 +52,152 @@ class AST:
 
     def __str__(self) -> str:
         return str(self.root)
+
+
+def convert_into_cnf(ast: AST) -> AST:
+    # 1. Convert to negation normal form
+    # 1.1. Eliminate implications, equivalences, and excludes
+    # 1.2. Move NOTs inwards by repeatdly applying De Morgan's Law and eliminate doble negations.
+    # 2. Distribute ORs invwards over ANDs, applying the Distribute property
+    ast =  eliminate_complex_operators(ast)
+
+
+def eliminate_implication(node: Node) -> Node:
+    """Replace P => Q with !P ∨ Q."""
+    pnot_node = Node("NOT")
+    pnot_node.left = node.left
+    new_node = Node("OR")
+    new_node.left = pnot_node
+    new_node.right = node.right
+    return new_node
+
+def eliminate_equivalence(node: Node) -> Node:
+    """Replace P <=> Q with (P ∨ !Q) ∧ (!P ∨ Q)."""
+    qnot_node = Node("NOT")
+    qnot_node.left = node.right 
+    pnot_node = Node("NOT")
+    pnot_node.left = node.left
+
+    left_node = Node("OR")
+    left_node.left = node.left 
+    left_node.right = qnot_node
+
+    right_node = Node("OR")
+    right_node.left = pnot_node 
+    right_node.right = node.right
+
+    new_node = Node("AND")
+    new_node.left = left_node
+    new_node.right = right_node
+    return new_node
+
+def eliminate_exclusion(node: Node) -> Node:
+    """Replace P => !Q with !P ∨ !Q."""
+    pnot_node = Node("NOT")
+    pnot_node.left = node.left
+    qnot_node = Node("NOT")
+    qnot_node.left = node.right
+    new_node = Node("OR")
+    new_node.left = pnot_node
+    new_node.right = qnot_node
+    return new_node
+
+def eliminate_complex_operators(ast: AST) -> AST:
+    """Eliminate imlications, equivalences, and excludes.
+
+    Repeatedly replace P => Q with !P ∨ Q; 
+    replace P <=> Q with (P ∨ !Q) ∧ (!P ∨ Q);
+    and replace P => !Q with !P ∨ !Q.
+    """
+    node = ast.root
+    stack = []
+    stack.append(node)
+    root = None
+    while stack:
+        n = stack.pop()
+        if n.is_op():
+            new_node = None
+            if n.data.upper() == 'REQUIRES' or n.data.upper() == 'IMPLIES':
+                new_node = eliminate_implication(n)
+            elif n.data.upper() == 'EQUIVALENCE':
+                new_node = eliminate_implication(n)
+            elif n.data.upper() == 'EXCLUDES':
+                new_node = eliminate_exclusion(n)
+            elif n.data.upper() == 'NOT':
+                stack.append(n.left)
+            else:  # OR, AND nodes
+                stack.append(n.left)
+                stack.append(n.right)
+
+            if new_node is not None:
+                stack.append(new_node.left)
+                stack.append(new_node.right)
+            
+            if root is None:
+                if new_node is not None:
+                    root = new_node
+                else:
+                    root = n
+    return AST(root)
+
+def apply_demorganlaw_or(node: Node) -> Node:
+    """Replace !(P ∨ Q) with (!P) ∧ (!Q)."""
+    pnot_node = Node("NOT")
+    pnot_node.left = node.left 
+    qnot_node = Node("NOT")
+    qnot_node.left = node.right
+    new_node = Node("AND")
+    new_node.left = pnot_node 
+    new_node.right = qnot_node
+    return new_node
+
+def apply_demorganlaw_and(node: Node) -> Node:
+    """Replace !(P ∧ Q) with (!P) ∨ (!Q)."""
+    pnot_node = Node("NOT")
+    pnot_node.left = node.left 
+    qnot_node = Node("NOT")
+    qnot_node.left = node.right
+    new_node = Node("OR")
+    new_node.left = pnot_node 
+    new_node.right = qnot_node
+    return new_node
+
+def move_nots_inwards(ast: AST) -> AST:
+    """Move NOTs inwards by repeatedly applying De Morgan's Law, and eliminate doble negations.
+    
+    Specifically, replace !(P ∨ Q) with (!P) ∧ (!Q);
+    replace !(P ∧ Q) with (!P) ∨ (!Q);
+    and replace !!P with P.
+    """
+    node = ast.root
+    stack = []
+    stack.append(node)
+    root = None
+    while stack:
+        n = stack.pop()
+        if n.is_op():
+            new_node = None
+            if n.data.upper() == 'NOT':
+                if n.left.is_op():
+                    if n.left.data.upper() == 'OR':
+                        new_node = apply_demorganlaw_or(n.left)
+                    elif n.left.data.upper() == 'AND':
+                        new_node = apply_demorganlaw_and(n.left)
+                    elif n.left.data.upper() == 'NOT':
+                        # Eliminate doble negation
+                        new_node = n.left.left
+                    else:  # OR, AND nodes
+                        stack.append(n.left)
+                        stack.append(n.right)
+
+            if new_node is not None:
+                stack.append(new_node.left)
+                stack.append(new_node.right)
+            
+            if root is None:
+                if new_node is not None:
+                    root = new_node
+                else:
+                    root = n
+
+    return AST(root)
