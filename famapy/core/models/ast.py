b85ed5677ea18a1624555f44557ff6744344d4dc
José Miguel Horcas <jhorcas@us.es>
2021-10-15 12:42:04 +0200

    Refactor AST operations as enum types

--- famapy/core/models/ast.py
+++ famapy/core/models/ast.py
@@ -1,10 +1,20 @@
-from typing import Optional
+from typing import Optional, Any
+from enum import Enum
+
+
+class ASTOperation(Enum):
+    REQUIRES = 'REQUIRES'
+    EXCLUDES = 'EXCLUDES'
+    AND = 'AND'
+    OR = 'OR'
+    IMPLIES = 'IMPLIES'
+    NOT = 'NOT'   
+    EQUIVALENCE = 'EQUIVALENCE'
 
 
 class Node:
-    operations = ['requires', 'excludes', 'and', 'or', 'implies', 'not', 'equivalence']
 
-    def __init__(self, data: str):
+    def __init__(self, data: Any):
         self.left: Optional['Node'] = None  # pylint: disable=unsubscriptable-object
         self.right: Optional['Node'] = None  # pylint: disable=unsubscriptable-object
         self.data = data
@@ -13,23 +23,21 @@ class Node:
         return not self.is_op()
 
     def is_op(self) -> bool:
-        ''' We check if the operation is available by comparing it on mayus '''
-        return (
-            isinstance(self.data, str) and
-            (self.data.upper() in (operation.upper() for operation in Node.operations))
-        )
+        return isinstance(self.data, ASTOperation)
 
     def __str__(self) -> str:
+        data = self.data.name if self.is_op() else self.data
+            
         if self.left and self.right:
-            return f'{self.data}[{self.left}][{self.right}]'
+            return f'{data}[{self.left}][{self.right}]'
 
         if self.left and not self.right:
-            return f'{self.data}[{self.left}][]'
+            return f'{data}[{self.left}][]'
 
         if not self.left and self.right:
-            return f'{self.data}[][{self.right}]'
+            return f'{data}[][{self.right}]'
 
-        return str(self.data)
+        return str(data)
 
 
 class AST:
@@ -38,14 +46,14 @@ class AST:
         self.root = root
 
     @classmethod
-    def create_simple_binary_operation(cls, operation: str, left: str, right: str) -> 'AST':
+    def create_simple_binary_operation(cls, operation: ASTOperation, left: str, right: str) -> 'AST':
         ast = cls(Node(operation))
         ast.root.left = Node(left)
         ast.root.right = Node(right)
         return ast
 
     @classmethod
-    def create_simple_unary_operation(cls, operation: str, elem: str) -> 'AST':
+    def create_simple_unary_operation(cls, operation: ASTOperation, elem: str) -> 'AST':
         ast = cls(Node(operation))
         ast.root.left = Node(elem)
         return ast
