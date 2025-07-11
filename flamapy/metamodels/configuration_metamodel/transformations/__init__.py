7a6b9fae260029e6d177dc66763fd1622c453d21
José Miguel Horcas <horcas@uma.es>
2025-07-11 11:54:25 +0200

    Feature: Basic configuration writer

--- flamapy/metamodels/configuration_metamodel/transformations/__init__.py
+++ flamapy/metamodels/configuration_metamodel/transformations/__init__.py
@@ -3,10 +3,12 @@ from .configuration_json_reader import ConfigurationJSONReader  # pylint: disabl
 from .uvls_json_reader import UVLSJSONReader  # pylint: disable=cyclic
 from .configuration_json_writer import ConfigurationJSONWriter  # pylint: disable=cyclic-import
 from .uvls_json_writer import UVLSJSONWriter  # pylint: disable=cyclic
+from .configuration_basic_writer import ConfigurationBasicWriter  # pylint: disable=cyclic-import
 
 
 __all__ = ['ConfigurationBasicReader',
            'ConfigurationJSONReader',
            'UVLSJSONReader',
            'ConfigurationJSONWriter',
-           'UVLSJSONWriter']
+           'UVLSJSONWriter',
+           'ConfigurationBasicWriter']
---
+++ flamapy/metamodels/configuration_metamodel/transformations/configuration_basic_writer.py
@@ -0,0 +1,30 @@
+import io
+import csv
+from typing import cast
+
+from flamapy.core.transformations.model_to_text import ModelToText
+from flamapy.core.models import VariabilityModel
+
+from flamapy.metamodels.configuration_metamodel.models.configuration import Configuration
+
+
+class ConfigurationBasicWriter(ModelToText):
+
+    @staticmethod
+    def get_destination_extension() -> str:
+        return 'csvconf'
+
+    def __init__(self, path: str, source_model: VariabilityModel) -> None:
+        self._path: str = path
+        self._configuration: Configuration = cast(Configuration, source_model)
+
+    def transform(self) -> str:
+        output = io.StringIO()
+        writer = csv.writer(output)
+        for key, value in self._configuration.elements.items():
+            writer.writerow([key, value])
+        csv_content = output.getvalue()
+        output.close()
+        with open(self._path, 'w', newline='', encoding='utf-8') as file:
+            file.write(csv_content)
+        return csv_content
